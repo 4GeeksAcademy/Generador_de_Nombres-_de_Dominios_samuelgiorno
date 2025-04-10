34bb7689962755f2e713c6131895ed2d660d85c0
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-04-10 18:28:55 +0200

    Update app.js
    cambio de let por const y cambio en el codigo, segunda entrega

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-// arrays
-let pronoun = ['the', 'our', 'his'];
-let adj = ['great', 'big', 'amazing'];
-let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
-
-// Generador de los dominios
-pronoun.forEach(p => {
-  adj.forEach(a => {
-    noun.forEach(n => {
-      console.log(`${p}${a}${n}.com`);
-    });
-  });
-});
+window.onload = function() {
+    // Arrays//
+    const pronoun = ['the', 'our', 'his'];
+    const adj = ['great', 'big', 'amazing'];
+    const noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
+    const dominion = [`.com`, `.net`, `.us`, `.io`];
+  
+    pronoun.forEach (p => {
+    adj.forEach (a => {
+    noun.forEach (n => {
+    dominion.forEach (d => {
+      console.log(`${p} ${a} ${n} ${d}`)
+      })})})}) 
+    };
